2542d681239fe977850c423fb31ccc6d1687e373
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-09-01 11:04:35 +0530

    Fix SQL query in user creation to correct the order of parameters

--- auth/authModels.js
+++ auth/authModels.js
@@ -3,8 +3,8 @@ const db = require("../db");
 class AuthModel {
   static async create(firstname, lastname, email, mobile, password, role = "user") {
     const [result] = await db.query(
-      "INSERT INTO users (firstname, lastname, email, mobile, password, role) VALUES (?, ?, ?, ?, ?, ?)",
-      [firstname, lastname, email, mobile, password, role]
+      "INSERT INTO users (firstname, lastname, mobile, email,  password, role) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, mobile, email, password, role]
     );
     return result.insertId;
   }
